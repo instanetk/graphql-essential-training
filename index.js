e33034c66f393d157d46beecf127e946367a03a6
David Quintero <davidq@gmail.com>
2021-02-23 13:10:12 -0500

    add getFriend resolver

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import schema from './schema';
 import { graphqlHTTP } from 'express-graphql';
+import resolvers from './resolvers';
 
 const app = express();
 
@@ -8,34 +9,7 @@ app.get('/', (req, res) => {
   res.send('GraphQL is amazing!');
 });
 
-const friendDatabase = {};
-
-class Friend {
-  constructor(id, { firstName, lastName, gender, email }) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.gender = gender;
-    this.email = email;
-  }
-}
-
-const root = {
-  friend: () => {
-    return {
-      id: 123456789,
-      firstName: 'David',
-      lastName: 'Quintero',
-      gender: 'male',
-      email: 'davidq@gmail.com',
-    };
-  },
-  createFriend: ({ input }) => {
-    let id = require('crypto').randomBytes(10).toString('hex');
-    friendDatabase[id] = input;
-    return new Friend(id, input);
-  },
-};
+const root = resolvers;
 
 app.use(
   '/graphql',
