99ebd39683ca5ac6ae1bd09619da5ba4e9e5f568
David Quintero <davidq@gmail.com>
2021-02-23 12:46:38 -0500

    GraphQL queries

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import schema from './schema';
+import { graphqlHTTP } from 'express-graphql';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('GraphQL is amazing!');
+});
+
+const root = {
+  friend: () => {
+    return {
+      id: 123456789,
+      firstName: 'David',
+      lastName: 'Quintero',
+      gender: 'male',
+      email: [{ email: 'davidq@gmail.com' }, { email: 'davidq@17style.com' }],
+    };
+  },
+};
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(8080, () => console.log('Running on localhost:8080/graphql'));
